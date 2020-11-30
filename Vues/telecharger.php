1c54b7e5a3a0c4fb7e3c3bff999262e1437e33b5
SGUENANA <guenanasarah@gmail.com>
2020-11-30 10:01:01 +0100

    Update telecharger.php

--- Vues/telecharger.php
+++ Vues/telecharger.php
@@ -2,7 +2,7 @@
 
 if($abonne== 1)
 {
-    echo "<a href='../Images/".$idFilm."' download='". $idFilm ."'>Télécharger</a>";
+    echo "<a href='Images/".$idFilm."' download='". $idFilm ."'>Télécharger</a>";
 }
 
 ?>
